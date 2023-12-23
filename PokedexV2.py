939689be35a7bc146fdd4adb1137091779e7fd9c
Joseph Casino <josephcasino88@gmail.com>
2023-12-23 16:18:15 -0600

    Turn Sprite Button

--- PokedexV2.py
+++ PokedexV2.py
@@ -35,8 +35,10 @@ class dexEntry(Toplevel):
 
         add_image_to_background_list(img)
         
+
         #Pokemon Information
         pokemon_info = my_canvas.create_text(375, 60, text=f" ", font=("Consolas", 12), fill= "black")
+        global pokemon_image
         pokemon_image = my_canvas.create_image(150,152)
         pokemon_type = my_canvas.create_text(375, 95, text=f" ", font=("Consolas", 12))
         pokemon_height = my_canvas.create_text(395, 185, text=f" ", font=("Consolas", 12), fill= "black")
@@ -49,20 +51,48 @@ class dexEntry(Toplevel):
         def add_image_to_global_list(image):
             global global_image_list
             global_image_list.append(image)
-
-
+    
         def load_pokemon(Pokemon_Entry):
             try: 
                 pokemon = pypokedex.get(name=Pokemon_Entry.get())
                 Pokemon.title(f"Pokedex Entry No.{pokemon.dex} - {pokemon.name}".title())
                 http = urllib3.PoolManager()
+                #get front sprite and display it
                 response = http.request('GET', pokemon.sprites.front.get('default'))
                 if response.status == 200:
                     image = PIL.Image.open(BytesIO(response.data))
                     resize_image = image.resize((200, 200))
-                    img = PIL.ImageTk.PhotoImage(resize_image)
-                    add_image_to_global_list(img)
+                    global  front_img
+                    front_img = PIL.ImageTk.PhotoImage(resize_image)
+                    add_image_to_global_list(front_img)
+                    my_canvas.itemconfig(pokemon_image, image=front_img)
+                #get back sprite
+                response = http.request('GET', pokemon.sprites.back.get('default'))
+                if response.status == 200:
+                    image2 = PIL.Image.open(BytesIO(response.data))
+                    resize_image2 = image2.resize((200, 200))
+                    global back_img
+                    back_img = PIL.ImageTk.PhotoImage(resize_image2)
+                    add_image_to_global_list(back_img)
                     
+                    #function for turn sprite button
+                    def switchsprite():
+                        global direction
+                        if direction == "front":
+                            my_canvas.itemconfig(pokemon_image, image=back_img)
+                            direction = "back"
+                            return
+                        if direction == "back":
+                            my_canvas.itemconfig(pokemon_image, image =front_img)
+                            direction = "front"
+                            return
+                        
+                    #switch sprite button and indicator
+                    global direction
+                    direction = "front"
+                    sprite_btn = Button(my_canvas, text= "TURN SPRITE", font=("Consolas", 8), width = 15, command=switchsprite)
+                    back_sprite_btn_window = my_canvas.create_window(66,50, window=sprite_btn)
+
                     #function for displaying the Pokemon Type(s)
                     def Type_Colors():
                         #changing pokemon.types list of strings so that it is just words                        
@@ -94,7 +124,7 @@ class dexEntry(Toplevel):
                                     return
                     
                         
-                    my_canvas.itemconfig(pokemon_image, image=img)
+                    #my_canvas.itemconfig(pokemon_image, image=front_img)
                     my_canvas.itemconfig(pokemon_info, text=f"No.{pokemon.dex} - {pokemon.name}".title())
                     Type_Colors()
                     my_canvas.itemconfig(pokemon_height, text=f"Height: {pokemon.height}0 cm")
@@ -111,11 +141,11 @@ class dexEntry(Toplevel):
                 Pokemon.title("MISSINGNO.")
                 image = PIL.Image.open("images\MissingNo.png")
                 resize_image = image.resize((200, 200))
-                img = PIL.ImageTk.PhotoImage(resize_image)
+                missingno_img = PIL.ImageTk.PhotoImage(resize_image)
                 add_image_to_global_list(img)
 
                 my_canvas.itemconfig(pokemon_info, text="MISSINGNO.")
-                my_canvas.itemconfig(pokemon_image, image=img)
+                my_canvas.itemconfig(pokemon_image, image=missingno_img)
                 my_canvas.itemconfig(pokemon_type, text="???? - ????")
                 my_canvas.itemconfig(pokemon_height, text="Height: ????")
                 my_canvas.itemconfig(pokemon_weight, text="Weight: ????")
@@ -125,11 +155,11 @@ class dexEntry(Toplevel):
             except Exception as e: #if an error occurred and who on earth knows what it was, just tell the user an error occurred
                 image = PIL.Image.open("images\MissingNo.png")
                 resize_image = image.resize((200, 200))
-                img = PIL.ImageTk.PhotoImage(resize_image)
+                missingno_img = PIL.ImageTk.PhotoImage(resize_image)
                 add_image_to_global_list(img)
 
                 my_canvas.itemconfig(pokemon_info, text="MISSINGNO.")
-                my_canvas.itemconfig(pokemon_image, image=img)
+                my_canvas.itemconfig(pokemon_image, image=missingno_img)
                 my_canvas.itemconfig(pokemon_type, text="???? - ????")
                 my_canvas.itemconfig(pokemon_height, text="Height: ????")
                 my_canvas.itemconfig(pokemon_weight, text="Weight: ????")
@@ -138,7 +168,6 @@ class dexEntry(Toplevel):
 
         load_pokemon(Pokemon_Entry)
 
-
 #Home Screen
 Home = tk.Tk()
 Home.geometry("360x640")
@@ -146,24 +175,24 @@ Home.resizable(width=False, height=False)
 Home.title("Python Pokedex")
 
 # Create Main Window Canvas
-my_canvas = Canvas(Home, width=360, height=640, bd=0, highlightthickness=0)
-my_canvas.pack(fill="both", expand=True)
+main = Canvas(Home, width=360, height=640, bd=0, highlightthickness=0)
+main.pack(fill="both", expand=True)
 
 # Set Pokedex background
 image = Image.open("images\Home.png")
 resize_image = image.resize((360, 640))
 img = PIL.ImageTk.PhotoImage(resize_image)
-c = my_canvas.create_image(0, 0, image=img, anchor="nw")
+c = main.create_image(0, 0, image=img, anchor="nw")
 
 #Add Texts
-title_text = my_canvas.create_text(180, 290, text="Python Pokedex", font=("Consolas", 30), fill="black")
-subtitle_text = my_canvas.create_text(180, 320, text="Created by JCPandaz", font=("Consolas", 15), fill="red")
-my_canvas.create_text(180, 420, text="Enter Name or Pokedex #:", font=("Consolas", 15), fill= "black")
+title_text = main.create_text(180, 290, text="Python Pokedex", font=("Consolas", 30), fill="black")
+subtitle_text = main.create_text(180, 320, text="Created by JCPandaz", font=("Consolas", 15), fill="red")
+main.create_text(180, 420, text="Enter Name or Pokedex #:", font=("Consolas", 15), fill= "black")
 #Add Entry box
 Pokemon_Entry = Entry(Home, font=("Consolas", 24), width=15, fg="black", bd=2)
-Pokemon_window=my_canvas.create_window(180,460, window=Pokemon_Entry)
+Pokemon_window=main.create_window(180,460, window=Pokemon_Entry)
 #Add Button
 btn = Button(Home, text= "LOAD POKEMON", font=("Consolas", 20), width = 18, command = dexEntry)
-load_btn_window = my_canvas.create_window(180,578, window=btn)
+load_btn_window = main.create_window(180,578, window=btn)
 
 Home.mainloop()
